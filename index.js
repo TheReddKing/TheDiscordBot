b7ff4a8cf01f4f4d431b894a2420677e8241e03d
Kevin Fang <reddacc@gmail.com>
2016-07-01 10:15:49 -0500

    Jokes are now live!

--- index.js
+++ index.js
@@ -8,18 +8,38 @@ GLOBAL.bot = new Discord.Client({
     autorun: true
 });
 
+String.prototype.replaceAll = function(search, replacement) {
+    var target = this;
+    return target.replace(new RegExp(search, 'g'), replacement);
+};
+
+
 bot.on('ready', function() {
     console.log(bot.username + " - (" + bot.id + ")");
 });
-
+String.prototype.replaceAll = function(search, replacement) {
+    var target = this;
+    return target.replace(new RegExp(search, 'g'), replacement);
+};
 //Bots
 var pingpong = require('./pingpong');
+var server = require('./server');
+var jokes = require('./jokes');
 var help = require('./help');
 bot.on('message', function(user, userID, channelID, message, event) {
     if (message === "=ping") {
         pingpong.pingpong(user,userID,channelID,message,event);
     }
+    if(message === "=mute") {
+        server.mute_id(user,userID,channelID,message,event);
+    }
     if (message === "=help") {
         help.help(user,userID,channelID,message,event);
+    }
+    if(message === "=badjoke") {
+        jokes.badjoke(user,userID,channelID,message,event);
+    }
+    if(message === "=badjoke --tts") {
+        jokes.badjokeTTS(user,userID,channelID,message,event);
     }
 });
--- jokes.js
+++ jokes.js
@@ -1,8 +1,35 @@
+var file = "data.db";
+var fs = require('fs');
+var exists = fs.existsSync(file);
+var sqlite3 = require("sqlite3").verbose();
+var db = new sqlite3.Database(file);
+var jokes = []
+db.serialize(function() {
+    db.each("SELECT id, text_title, text_joke FROM s001_redditjokes", function(err, row) {
+        // console.log(row.id + ": " + row.text_title);
+        jokes.push("\n\n" + row.text_title + " !THEREDDKING! " + row.text_joke);
+    });
+});
+
 module.exports = {
     badjoke: function (user,userID,channelID,message,event) {
         bot.sendMessage({
             to: channelID,
-            message: "<@" + userID + "> What do you call an person? --- A fish"
+            message: "<@" + userID + ">" + randomJoke()
+        });
+    },
+    badjokeTTS: function (user,userID,channelID,message,event) {
+        bot.sendMessage({
+            to: channelID,
+            message: "<@" + userID + ">"
+        });
+        bot.sendMessage({
+            to: channelID,
+            message: randomJoke(),
+            tts: true
         });
     }
 };
+function randomJoke() {
+    return  jokes[parseInt(Math.random()*jokes.length)].replaceAll("\\|\\|","\n").replaceAll(" !THEREDDKING! ","\n\n\n")
+}
